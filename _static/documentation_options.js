076e3465871bf7ffc274b7632eac237d595040bf
AdamGagorik <AdamGagorik@users.noreply.github.com>
2022-04-20 01:43:37 +0000

    deploy: bf4696cafccca37a1473257a5b7252cf7cc2208b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0.post421.dev0+349b76d',
+    VERSION: '0.0.0.post424.dev0+bf4696c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
